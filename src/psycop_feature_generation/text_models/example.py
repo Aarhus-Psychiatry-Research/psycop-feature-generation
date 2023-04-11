f3b3d30c7154afda0a9f5bfd67565dfca3822136
signekb <signebroedbaek@gmail.com>
2023-04-11 16:33:55 +0200

    Revert "feat: fit and load tfidf, bow, and lda models"
    This reverts commit 3d33d9b2a74f02afb375af71e287f2e01b8af19d.

--- src/psycop_feature_generation/text_models/example.py
+++ src/psycop_feature_generation/text_models/example.py
@@ -2,7 +2,10 @@ from psycop_feature_generation.text_models.preprocessing import tfidf_preprocess
 
 if __name__ == "__main__":
     df = tfidf_preprocessing(
-        text_sfi_names="Aktuelt psykisk", n_rows=10, split_name="train"
+        text_sfi_names="Aktuelt psykisk", n_rows=10, split_name=["train", "val"],
     )
 
     df
+
+revert 29b442b39be78071614b584f420d6cdde0fa80d7
+d2861665585bcb9d8cf5f3e9023f59ddd038787b
